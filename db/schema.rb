e157a39889408106513d52476c06c61eb48e085d
yuliyambg <yuliyambg@gmail.com>
2021-09-17 19:28:58 -0400

    adding bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_162350) do
+ActiveRecord::Schema.define(version: 2021_09_17_231813) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
