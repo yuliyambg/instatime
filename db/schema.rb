beabc8faf23046c49be6d2dd3f63f17d84249447
yuliyambg <yuliyambg@gmail.com>
2021-09-18 16:35:22 -0400

    adding views for post new and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_231813) do
+ActiveRecord::Schema.define(version: 2021_09_18_183643) do
 
   create_table "posts", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
